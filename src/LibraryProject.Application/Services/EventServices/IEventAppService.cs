e2d4dcaa2baafa8cb0a7a1920328f9120af7e674
Rea <89607378+Areyeng@users.noreply.github.com>
2024-03-14 15:55:29 +0200

    I added more cruds and entities.

---
+++ src/LibraryProject.Application/Services/EventServices/IEventAppService.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Application.Services;
+using LibraryProject.Services.EventServices.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Services.EventServices
+{
+    public interface IEventAppService : IApplicationService
+    {
+        Task<EventDto> CreateEventAsync(EventDto input);
+        Task<EventDto> UpdateEventAsync(EventDto input);
+        Task Delete(Guid id);
+        Task<EventDto> GetByEventId(Guid id);
+        Task<List<EventDto>> GetAllEventAsync();
+
+    }
+}
---
+++ src/LibraryProject.Application/Services/PersonServices/Dtos/PersonUpdateDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Services.PersonServices.Dtos
+{
+    public class PersonUpdateDto  
+    {
+        public virtual string Name { get; set; }
+        public virtual string Surname { get; set; }
+        public virtual int Age { get; set; }
+        public virtual string Email { get; set; }
+        public virtual int RoleId { get; set; }
+
+        public virtual string Occupation { get; set; }
+        public virtual string IdNumber { get; set; }
+        public virtual string PassportNumber { get; set; }
+
+        public virtual long UserId { get; set; }
+    }
+}
